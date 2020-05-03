5644f02906ce813cf395b16be88f33e4208198cb
Alexandru310798 <36679435+Alexandru310798@users.noreply.github.com>
2020-05-03 15:01:32 +0300

    commit

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -3,10 +3,10 @@ from itertools import permutations
 from tkinter import *
 from tkinter.messagebox import showinfo
 
-available_positions = [11, 12, 13, 21, 22, 23, 31, 32, 33]
-# buttons for the GUI
-root = Tk()
-button_1_1 = Button(root, width=20, height=10, command=lambda: define_sign(11))
+available_positions = [11, 12, 13, 21, 22, 23, 31, 32, 33]  # the positions of the table
+
+root = Tk()  # root of the GUI
+button_1_1 = Button(root, width=20, height=10, command=lambda: define_sign(11))  # buttons for the GUI
 button_1_2 = Button(root, width=20, height=10, command=lambda: define_sign(12))
 button_3_3 = Button(root, width=20, height=10, command=lambda: define_sign(33))
 button_3_2 = Button(root, width=20, height=10, command=lambda: define_sign(32))
@@ -16,17 +16,18 @@ button_3_1 = Button(root, width=20, height=10, command=lambda: define_sign(31))
 button_2_1 = Button(root, width=20, height=10, command=lambda: define_sign(21))
 button_1_3 = Button(root, width=20, height=10, command=lambda: define_sign(13))
 
+# default values
 AI_wins = 0
 player_wins = 0
-player = []
-AI = []
+player = []  # player moves
+AI = []  # AI moves
 AI_turn = False
 AI_sign = 'O'
 player_sign = 'X'
 alphabeta_choice = 0
 
 
-def lost(list_of_positions=[]):
+def lost(list_of_positions=[]):  # This is the function to verify if someone lose( win in the regular Tic Tac Toe)
     if list_of_positions.count(11) > 0 and list_of_positions.count(12) > 0 and list_of_positions.count(13) > 0:
         return True
     if list_of_positions.count(21) > 0 and list_of_positions.count(22) > 0 and list_of_positions.count(23) > 0:
@@ -46,42 +47,44 @@ def lost(list_of_positions=[]):
     return False
 
 
-def winner(list_of_positions=[]):
+def winner(
+        list_of_positions=[]):  # being a Reverse Tic Tac Toe, the winning rule, is the opposite of the regular Tic Tac Toe
     valid = 0
     if not available_positions:
         valid += 1
     if not (list_of_positions.count(11) > 0 and list_of_positions.count(12) > 0 and list_of_positions.count(13) > 0):
         valid += 1
-    if not(list_of_positions.count(21) > 0 and list_of_positions.count(22) > 0 and list_of_positions.count(23) > 0):
+    if not (list_of_positions.count(21) > 0 and list_of_positions.count(22) > 0 and list_of_positions.count(23) > 0):
         valid += 1
-    if not(list_of_positions.count(31) > 0 and list_of_positions.count(32) > 0 and list_of_positions.count(33) > 0):
+    if not (list_of_positions.count(31) > 0 and list_of_positions.count(32) > 0 and list_of_positions.count(33) > 0):
         valid += 1
-    if not(list_of_positions.count(11) > 0 and list_of_positions.count(21) > 0 and list_of_positions.count(31) > 0):
+    if not (list_of_positions.count(11) > 0 and list_of_positions.count(21) > 0 and list_of_positions.count(31) > 0):
         valid += 1
-    if not(list_of_positions.count(12) > 0 and list_of_positions.count(22) > 0 and list_of_positions.count(32) > 0):
+    if not (list_of_positions.count(12) > 0 and list_of_positions.count(22) > 0 and list_of_positions.count(32) > 0):
         valid += 1
-    if not(list_of_positions.count(13) > 0 and list_of_positions.count(23) > 0 and list_of_positions.count(33) > 0):
+    if not (list_of_positions.count(13) > 0 and list_of_positions.count(23) > 0 and list_of_positions.count(33) > 0):
         valid += 1
-    if not(list_of_positions.count(11) > 0 and list_of_positions.count(22) > 0 and list_of_positions.count(33) > 0):
+    if not (list_of_positions.count(11) > 0 and list_of_positions.count(22) > 0 and list_of_positions.count(33) > 0):
         valid += 1
-    if not(list_of_positions.count(13) > 0 and list_of_positions.count(22) > 0 and list_of_positions.count(31) > 0):
+    if not (list_of_positions.count(13) > 0 and list_of_positions.count(22) > 0 and list_of_positions.count(31) > 0):
         valid += 1
 
-    if valid == 9:
+    if valid == 9:  # If all "losing" rules are false, the function will return True
         return True
     return False
 
 
 def minimax(positions, depth, artificial_intelligence, a_i_positions=[], player_positions=[]):
     if not positions:
-        if winner(a_i_positions):
+        if winner(a_i_positions):  # if the AI can win in this state, it'll receive 10 pts - depth
             return dict(position=None, score=10 - depth)
-        elif winner(player_positions):
+        elif winner(player_positions):  # if the AI lose in this state, it'll receive - 10 pts - depth
             return dict(position=None, score=-10 - depth)
         else:
-            return dict(position=None, score=0 - depth)
+            return dict(position=None,
+                        score=0 - depth)  # if none of them win in this state, the AI will receive 0 pts - depth
 
-    if artificial_intelligence:
+    if artificial_intelligence:  # this search is for the AI, so search for the max value
         best = {'position': None, 'score': (-1) * math.inf}
         for possible_move in positions:
             positions.remove(possible_move)
@@ -93,7 +96,7 @@ def minimax(positions, depth, artificial_intelligence, a_i_positions=[], player_
             a_i_positions.remove(possible_move)
             positions.append(possible_move)
 
-    else:
+    else:  # this search is for the player, so the AI will try to find the min value
         best = {'position': None, 'score': math.inf}
         for possible_move in positions:
             player.append(possible_move)
@@ -104,11 +107,11 @@ def minimax(positions, depth, artificial_intelligence, a_i_positions=[], player_
                 best = value
             positions.append(possible_move)
             player.remove(possible_move)
-    return best
+    return best  # the best returned will be :{'position': x, 'score': maximum available}, so we will use that position
 
 
 def alphabeta(positions, depth, artificial_intelligence, a_i_positions=[], player_positions=[], alpha=(-1) * math.inf,
-              beta=math.inf):
+              beta=math.inf):  # the alpha beta prunning is same as minimax, except that here will take care of 2 new variables
     if not positions:
         if winner(a_i_positions):
             return dict(position=None, score=10 - depth)
@@ -126,11 +129,13 @@ def alphabeta(positions, depth, artificial_intelligence, a_i_positions=[], playe
             value['position'] = possible_move
             if best['score'] < value['score']:
                 best = value
-            alpha = max(alpha, value['score'])
-            if alpha >= beta:
-                break
             a_i_positions.remove(possible_move)
             positions.append(possible_move)
+            alpha = max(alpha,
+                        value['score'])  # if the alpha conditions will be true, will stop searching for useless values
+            if alpha >= beta:
+                break
+
 
     else:
         best = {'position': None, 'score': math.inf}
@@ -141,31 +146,32 @@ def alphabeta(positions, depth, artificial_intelligence, a_i_positions=[], playe
             value['position'] = possible_move
             if best['score'] > value['score']:
                 best = value
+            positions.append(possible_move)
+            player.remove(possible_move)
             beta = min(beta, value['score'])
             if beta <= alpha:
                 break
-            positions.append(possible_move)
-            player.remove(possible_move)
+
     return best
 
 
 def select_algorithm(positions, depth, artificial_intelligence, a_i_positions=[], player_positions=[],
-                     alphabeta_choice=False):
+                     alphabeta_choice=False):  # this function is used to select wich algorithm we will compete against
     if alphabeta_choice:
         return alphabeta(positions, depth, True, a_i_positions, player_positions, (-1) * math.inf, math.inf)
     return minimax(positions, depth, True, a_i_positions, player_positions)
 
 
-def define_sign(number, sign='X'):
+def define_sign(number, sign='X'):  # this is the click function of the GUI Buttons
     global player, AI, AI_wins, player_wins
     global AI_turn, alphabeta_choice
-    if available_positions.count(number) == 0:
+    if available_positions.count(number) == 0:  # if no more moves, end game
         return
-    if AI_turn:
+    if AI_turn:  # determine who is to move
         sign = AI_sign
     else:
         sign = player_sign
-    if number == 11:
+    if number == 11:  # for every button, we'll check who is to move an will add the position in his array
         if AI_turn:
             AI.append(number)
         else:
@@ -220,31 +226,32 @@ def define_sign(number, sign='X'):
             player.append(number)
         button_3_3.config(text=sign)
 
-    available_positions.remove(number)
+    available_positions.remove(number)  # we'll remove the position from available position because it was taken
     AI_turn = not AI_turn
 
-    if AI_turn:
-        AI_move = minimax(available_positions, 0, True, AI, player)['position']
+    if AI_turn:  # if we moved, now it's AI time to move, and it'll choose
+        AI_move = select_algorithm(available_positions, 0, True, AI, player, alphabeta_choice)['position']
         define_sign(AI_move)
 
-    if lost(player):
+    if lost(player):  # verify if someone has lost, to can stop the game
         showinfo("Game result", "Player has lost, AI has won")
         AI_wins += 1
     elif lost(AI):
         showinfo("Game result", "AI has lost, Player has won")
         player_wins += 1
-    elif not (available_positions or lost(player) or lost(AI) ):
+    elif not (available_positions or lost(player) or lost(AI)):
         showinfo("Game result", "It's a draw")
 
 
-def reverse_sign(sign):
+def reverse_sign(sign):  # the reverse sign
     if sign == 'X':
         return '0'
     else:
         return 'X'
 
 
-def draw_table(artificial=[], artificial_sign='X', player1=[], player1_sign='O'):
+def draw_table(artificial=[], artificial_sign='X', player1=[],
+               player1_sign='O'):  # the drawing table function  for the console playing
     table = []
     for i in range(34):
         if artificial.count(i):
@@ -268,53 +275,60 @@ def draw_table(artificial=[], artificial_sign='X', player1=[], player1_sign='O')
     print(string)
 
 
-def play(positions, artificial, player1):
+def play(positions, artificial, player1):  # playing console function
     global AI_sign, player_sign, alphabeta_choice
-    print("If you want to exit the game, please insert an invalid number")
-    while positions or winner(player1) or winner(artificial):
+    print("If you want to exit the game, please insert an invalid number")  # if you want to end the game
+    while positions or winner(player1) or winner(artificial):  # while we can still play, we play
         your_move = int(input("Your move: "))
-        if available_positions.count(your_move) == 0:
+        if available_positions.count(your_move) == 0:  # check 'till the move is valid
             while available_positions.count(your_move) == 0:
                 exit_option = input("If you want to close the game, just type 'exit' :")
                 if exit_option == "exit":
                     exit(0)
                 your_move = int(input("Your move was already taken, please choose another: "))
 
-        player1.append(your_move)
-        positions.remove(your_move)
+        player1.append(your_move)  # adding the position
+        positions.remove(your_move)  # remove the position
 
-        if not positions:
+        if not positions:  # if that was the last available position
             break
 
-        artificial_move = select_algorithm(positions, 0, True, artificial, player1)['position']
+        artificial_move = select_algorithm(positions, 0, True, artificial, player1, alphabeta_choice)[
+            'position']  # here we choose the search algorithm we are facing
 
         print("AI move is: " + str(artificial_move))
         artificial.append(artificial_move)
         positions.remove(artificial_move)
 
-        draw_table(artificial, AI_sign, player1, player_sign)
+        draw_table(artificial, AI_sign, player1, player_sign)  # show me how the table looks like
         if lost(player1):
-            print("Player has lost, AI has won")
+            print("Player has lost, AI has won")  # determine the winner
             exit(0)
         elif lost(artificial):
             print("AI has lost, Player has won")
             exit(0)
 
-    print("It's a draw")
+            # the last check
+    if lost(player1):
+        print("Player has lost, AI has won")
+    elif lost(AI):
+        print("AI has lost, Player has won")
+    else:
+        print("It's a draw")
     draw_table(artificial, AI_sign, player1, player_sign)
 
 
 if __name__ == "__main__":
 
-    print(winner([31, 33, 32, 22, 21, 11]))
-    print("You will compete against:\n 1) MiniMax Algorithm \n 2) AlphaBeta Prunning\n your choice is( 1 / 2): ")
+    # You'll have to choose how to play
+    print("You will compete against:\n 1) MiniMax Algorithm \n 2) AlphaBeta Pruning\n your choice is( 1 / 2): ")
     alphabeta_choice = int(input())
     player_sign = input("Your sign is( X/ 0): ")
     AI_sign = reverse_sign(player_sign)
     if alphabeta_choice == 1:
-        print("You will compete aganinst the MiniMax Algorithm and your sign is: " + player_sign)
+        print("You will compete against the MiniMax Algorithm and your sign is: " + player_sign)
     else:
-        print("You will compete aganinst the Alpha Beta Prunning Algorithm and your sign is: " + player_sign)
+        print("You will compete against the Alpha Beta Prunning Algorithm and your sign is: " + player_sign)
 
     console_line_or_GUI = input("The way to play: console/ GUI: ")
     i = 0
@@ -336,10 +350,8 @@ if __name__ == "__main__":
     player.clear()
     AI.clear()
 
-
     if console_line_or_GUI == "GUI":
         while available_positions:
             root.mainloop()
-
     else:
         play(available_positions, AI, player)
